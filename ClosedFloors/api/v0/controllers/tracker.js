29ba8b3a797641c0edc87f9977ad343e5c6a59bc
lunaroyster <oneinaquadrillion@live.com>
2016-12-01 08:23:43 +0000

    Trackers

--- ClosedFloors/api/v0/controllers/tracker.js
+++ ClosedFloors/api/v0/controllers/tracker.js
@@ -1,21 +1,25 @@
 var trackerCore = require('../core/tracker');
 
 module.exports.getTrackers = function(req, res) {
-    res.status(200);
-    res.send("Trackers for Service " + req.serviceID);
+    trackerCore.getTrackersByService(req.serviceID, function(err, trackers){
+        res.json(trackers);
+    })
 };
 
 module.exports.getATracker = function(req, res) {
-    res.send("Tracker " + req.trackerID + " for Service " + req.serviceID);
+    trackerCore.getTrackerByIDs(req.serviceID, req.trackerID, function(err, tracker) {
+        res.json(tracker);
+    });
 };
 module.exports.createATracker = function(req, res) {
     req.trackerName = req.query.trackerName;
-    trackerCore.createTracker(req.serviceID, req.trackerName, function(err, response) {
+    req.trackerStatus = req.query.trackerStatus;
+    trackerCore.createTracker(req.serviceID, req.trackerName, req.trackerStatus, function(err, response) {
         res.json(response);
     });
 };
 module.exports.updateATracker=function(req, res) {
-    res.send("Tracker " + req.trackerID + " updated for Service " + req.serviceID);
+    // res.send("Tracker " + req.trackerID + " updated for Service " + req.serviceID);
 };
 module.exports.trackerIDAppend = function(req, res, next) {
     req.trackerID = req.params.trackerID;
