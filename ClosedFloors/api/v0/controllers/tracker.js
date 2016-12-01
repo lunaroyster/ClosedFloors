a82109a223fff384ce0b58f3a0cfb8df40b95d2c
lunaroyster <oneinaquadrillion@live.com>
2016-12-01 07:51:38 +0000

    Tracker code

--- ClosedFloors/api/v0/controllers/tracker.js
+++ ClosedFloors/api/v0/controllers/tracker.js
@@ -1,3 +1,5 @@
+var trackerCore = require('../core/tracker');
+
 module.exports.getTrackers = function(req, res) {
     res.status(200);
     res.send("Trackers for Service " + req.serviceID);
@@ -7,7 +9,10 @@ module.exports.getATracker = function(req, res) {
     res.send("Tracker " + req.trackerID + " for Service " + req.serviceID);
 };
 module.exports.createATracker = function(req, res) {
-    res.send("Tracker created for Service " + req.serviceID);
+    req.trackerName = req.query.trackerName;
+    trackerCore.createTracker(req.serviceID, req.trackerName, function(err, response) {
+        res.json(response);
+    })
 }
 module.exports.updateATracker=function(req, res) {
     res.send("Tracker " + req.trackerID + " updated for Service " + req.serviceID);
