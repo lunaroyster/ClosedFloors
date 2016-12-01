2fdb90ae4c31612828c7e21125f8a925d882dbe6
lunaroyster <oneinaquadrillion@live.com>
2016-12-01 05:25:25 +0000

    Updated routes

--- ClosedFloors/api/v0/controllers/service.js
+++ ClosedFloors/api/v0/controllers/service.js
@@ -5,7 +5,9 @@ module.exports.getServices = function(req, res) {
 module.exports.getAService = function(req, res) {
     res.send("Get service: " + req.serviceID);
 };
-
+module.exports.updateAService = function(req, res) {
+    res.send("Update Service " + req.serviceID);
+};
 module.exports.serviceIDAppend = function(req, res, next) {
     req.serviceID = req.params.serviceID;
     console.log(req.serviceID);
--- ClosedFloors/api/v0/controllers/tracker.js
+++ ClosedFloors/api/v0/controllers/tracker.js
@@ -6,7 +6,12 @@ module.exports.getTrackers = function(req, res) {
 module.exports.getATracker = function(req, res) {
     res.send("Tracker " + req.trackerID + " for Service " + req.serviceID);
 };
-
+module.exports.createATracker = function(req, res) {
+    res.send("Tracker created for Service " + req.serviceID);
+}
+module.exports.updateATracker=function(req, res) {
+    res.send("Tracker " + req.trackerID + " updated for Service " + req.serviceID);
+}
 module.exports.trackerIDAppend = function(req, res, next) {
     req.trackerID = req.params.trackerID;
     console.log(req.trackerID);
