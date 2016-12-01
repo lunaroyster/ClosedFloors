980036f607e9c7ca2d84264452862d8cfa7e8365
lunaroyster <oneinaquadrillion@live.com>
2016-12-01 03:36:59 +0000

    Controller for the search page

--- ClosedFloors/public/js/main.js
+++ ClosedFloors/public/js/main.js
@@ -26,3 +26,40 @@ app.config(function($routeProvider) {
     .otherwise({redirectTo: '/'});
     
 });
+
+app.controller('SearchController', function($scope, $http, $location) {
+    $scope.loc = {
+       latitude: 0,
+       longitude: 0
+    };
+    $scope.selected = {
+        service: ""
+    };
+    $scope.getloc = function() {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                $scope.loc.latitude = position.coords.latitude;
+                $scope.loc.longitude = position.coords.longitude;
+                console.log($scope.loc.latitude, $scope.loc.longitude);
+                $scope.$digest();
+            });
+        }
+        
+    };
+    $scope.getResults = function() {
+        $http({
+            method: 'GET',
+            url: '/api/v0/service',
+            params: {
+                'latitude': $scope.loc.latitude,
+                'longitude': $scope.loc.longitude,
+                'serviceType': $scope.selected.service
+            }
+        }).then(function successCallback(response) {
+            searchResults = response.data;
+            $location.path('/result');
+        }, function errorCallback(response) {
+            console.log(response);
+        });
+    };
+});
